42ea50f2e98adbeb29d02ec32f6b7e6c99f52cbc
FolivoraDev <FolivoraDev@gmail.com>
2018-11-08 19:15:53 +0900

    9

--- app/members/models.py
+++ app/members/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
     # admin에서 이 필드를 수정할 수 있도록 설정
     img_profile = models.ImageField(upload_to='user', blank=True)
 
+    nickname = models.CharField(max_length=20, unique=True, blank=True, null=True)
+
     class Meta:
         verbose_name = '사용자'
         verbose_name_plural = verbose_name + ' 목록'
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git add -f .secrets/
-eb deploy --profile EB --staged
+eb deploy --profile EB --staged & sleep 3
 git reset HEAD .secrets/
