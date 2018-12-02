94c50e790f1d84d5f5165d8bc4c60257e8a45d3d
FolivoraDev <FolivoraDev@gmail.com>
2018-12-02 12:41:41 +0900

    nginx수정

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -30,6 +30,7 @@ AUTH_USER_MODEL = 'members.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',)
