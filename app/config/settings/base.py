a4b66c96eb254f806829e451dec261625fbcff47
FolivoraDev <FolivoraDev@gmail.com>
2018-12-03 17:24:24 +0900

    미들웨어 다시 추가

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'config.middleware.MyCORSMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
 
